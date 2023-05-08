91661eaac63c84c3975ae11c9596e07d30848a5a
Venelin Efremov <ghent360@iqury.us>
2023-05-08 00:50:08 -0700

    Revert "Add comments. Rename GPIO_Write."
    This reverts commit 0296ee2cf8b18da545308ca046e0ed02d801ec32.

--- examples/blink/blink.c
+++ examples/blink/blink.c
@@ -14,19 +14,7 @@ static inline void GPIO_Configure_Pin(
 	port->CFGLR = port_cfg;
 }
 
-// the on and off masks indicate which port pins to turn on or off.
-// mask value of 0 means don't turn anything on or off. should not 
-// duplicate pins in the on and off mask or behavior is undefined.
-//
-// Mask format: bit 0 - pin 0, bit 1 - pin 1 etc. For example mask
-// 0b01001001 - means pins 6, 3 and 0 of the corresponding port.
-//
-// This works on the pins of a single port only. pins masked with 0 are 
-// unchanged. GPIO_Write(x, 0, 0) is a noop.
-//
-// Make sure to write to output pins only.
-//
-static inline void GPIO_WriteMask(
+static inline void GPIO_Write(
 	GPIO_TypeDef* port, uint8_t on_mask, uint8_t off_mask) {
 	port->BSHR = on_mask | (off_mask << 16);
 }
@@ -51,11 +39,11 @@ int main()
 	int32_t inc_dec = 1;
 	while(1)
 	{
-		GPIO_WriteMask(GPIOD, 0b00000001, 0b00010000); // Turn on PD0, off PD4
-		//GPIO_WriteMask(GPIOC, 0b00000001, 0); 		   // Turn on PC0
+		GPIO_Write(GPIOD, 0b00000001, 0b00010000); // Turn on PD0, off PD4
+		//GPIO_Write(GPIOC, 0b00000001, 0); 		   // Turn on PC0
 		Delay_Ms( 250 - count );
-		GPIO_WriteMask(GPIOD, 0b00010000, 0b00000001); // Turn off PD0, on PD4
-		//GPIO_WriteMask(GPIOC, 0, 0b00000001);		   // Off PC0
+		GPIO_Write(GPIOD, 0b00010000, 0b00000001); // Turn off PD0, on PD4
+		//GPIO_Write(GPIOC, 0, 0b00000001);		   // Off PC0
 		Delay_Ms( count );
 		count += inc_dec;
 		if( count > 250 || count == 0 ) {
